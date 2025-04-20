8bbe1a458564e9e5594f4fc8ff7752b8f6ca5536
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2025-04-20 23:30:57 +0100

    added edpt-dark

--- lua/kickstart/plugins/onedark.lua
+++ lua/kickstart/plugins/onedark.lua
@@ -1,13 +1,13 @@
 -- make sure to commen out "vim.vmd.colorscheme 'tokyonight'" r the theme might not load
 
 return {
-  {
-    'mawkler/onedark.nvim',
-    priority = 1000,
-    init = function()
-      vim.cmd.colorscheme 'onedark'
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+	{
+		"S1lverCr0w/edpt-dark.nvim",
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("onedark")
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
 }
 -- vim: ts=2 sts=2 sw=2 et- See the kickstart.nvim README for more information
