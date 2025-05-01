73455bee6643401f59fc6184776d4a3c216aed5c
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2025-05-02 00:31:40 +0100

    clean cache script

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+usr_dir="/home/enryu"
+
+rm -rf "$usr_dir/.cargo/"
+
+rm -rf "$usr_dir/.cache/"
+
+rm -rf "$usr_dir/.gradle/caches/"
+
+rm -rf "$usr_dir/.local/share/gnome-builder/"
+
+rm -rf "$usr_dir/.local/share/rustup/"
+
+rm -rf "$usr_dir/.local/share/yarn/"
+
+rm -rf "$usr_dir/.local/share/yarn/"
