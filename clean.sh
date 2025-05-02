a8a0b9245e23affe30d8f7aa963528ecd93b3d76
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2025-05-02 19:52:04 +0100

    fixed where the dricetories would not delete because of the *

--- clean.sh
+++ clean.sh
@@ -2,16 +2,20 @@
 
 usr_dir="/home/enryu"
 
-rm -rf "$usr_dir/.cargo/*"
+rm -rf "$usr_dir/.cargo/"*
 
-rm -rf "$usr_dir/.cache/*"
+rm -rf "$usr_dir/.cache/"*
 
-rm -rf "$usr_dir/.gradle/caches/*"
+rm -rf "$usr_dir/.gradle/caches/"*
 
-rm -rf "$usr_dir/.local/share/gnome-builder/*"
+rm -rf "$usr_dir/.local/share/gnome-builder/"*
 
-rm -rf "$usr_dir/.local/share/rustup/*"
+rm -rf "$usr_dir/.local/share/rustup/"*
 
-rm -rf "$usr_dir/.local/share/yarn/*"
+rm -rf "$usr_dir/.local/share/yarn/"*
 
-rm -rf "$usr_dir/.local/share/yarn/*"
+rm -rf "$usr_dir/.local/share/yarn/"*
+
+journalctl --disk-usage #show how much storage logs take
+#echo "type password to keep logs for 7 days max"
+#doas journalctl --vacuum-time=7d #keep logs only for 7 days
