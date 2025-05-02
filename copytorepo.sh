7864ba872bd0297238200f514eb669c1afa86ef5
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2025-05-02 19:38:00 +0100

    testing copytorepo script

--- copytorepo.sh
+++ copytorepo.sh
@@ -1,9 +1,61 @@
 #!/usr/bin/env bash
 
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+
+# nvim #
+nvim() {
+	cp -r $HOME/.config/nvim/* nvim
+}
+
+
+# alacritty #
+alacritty() {
+	cp -r $HOME/.config/alacritty/* alacritty
+}
+
+
+# helix #
+helix() {
+	cp -r $HOME/.config/helix/* helix
+}
+
+
+# nano #
+nano() {
+	echo "$script_dir"
+	cp $HOME/.config/nano/* $script_dir
+}
+
+
+# hyprland #
+hyprland() {
+	cp -r $HOME/.config/hypr/* hypr
+}
+
 
 # waybar #
 waybar() {
 	cp -r $HOME/.config/waybar/* waybar
 }
 
-waybar
+
+# configs #
+configs() {
+	cp -r $HOME/.bashrc bashrc
+	cp -r $HOME/.bash_profile bash_profile
+}
+
+
+main() {
+	nvim
+	alacritty
+	helix
+	nano
+	hyprland
+	waybar
+	configs
+}
+
+
+main
