963978733f49df9824a82ff6f51bdaac3eb11c7b
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2025-12-19 20:46:02 +0000

    added jdlts plugin (java) and some other changes

--- config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -221,11 +221,11 @@ return {
 			local servers = {
 				clangd = {},
 				zls = {},
-				gopls = {},
+				-- gopls = {},
 				pyright = {},
 				rust_analyzer = {},
 				-- asm_lsp = {},
-				bashls = {},
+				-- bashls = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
