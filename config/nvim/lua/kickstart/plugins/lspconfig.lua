2becb13db66b2a8fc98466480a14ea6bd00f2d13
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2025-12-22 14:40:15 +0000

    added fortls

--- config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -224,8 +224,9 @@ return {
 				-- gopls = {},
 				pyright = {},
 				rust_analyzer = {},
-				-- asm_lsp = {},
-				-- bashls = {},
+				asm_lsp = {},
+				bashls = {},
+				fortls = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
