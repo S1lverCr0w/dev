b657660c7ecbefce1d5fa9cd4cc8d2237d2c994b
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2026-01-21 17:14:36 +0000

    nvim new setup 13

--- config/nvim/lua/plugins/blink.lua
+++ config/nvim/lua/plugins/blink.lua
@@ -15,7 +15,26 @@ return {
 			nerd_font_variant = "mono",
 		},
 
-		completion = { documentation = { auto_show = true, auto_show_delay_ms = 500 } },
+		completion = {
+			-- ghost_text = {
+			-- 	enabled = true,
+			-- },
+			menu = {
+				border = "single",
+				scrollbar = true,
+
+				-- max_height = 20,
+				-- auto_show = false, -- hide menu if ghost text is enabled
+			},
+			documentation = {
+				auto_show = false,
+				auto_show_delay_ms = 500,
+				window = {
+					border = "single",
+					scrollbar = true,
+				},
+			},
+		},
 
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
