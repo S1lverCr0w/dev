e2279a905567320a5d20b1960a1188adf118213f
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2026-01-27 17:48:28 +0000

    nvim new setup 31

--- setup.sh
+++ setup.sh
@@ -63,7 +63,6 @@ nvim() {
 	[ -d $HOME/.config/nvim/ ] &&  log mv $HOME/.config/nvim $HOME/.config/nvim_$datetime
 	# [ -d $HOME/.local/share/nvim/ ] &&  log mv $HOME/.local/share/nvim $HOME/.local/share/nvim_$datetime
 	nvim_clear
-	log rm -rf $HOME/.cache/nvim
 
 	log mkdir -p "$HOME/.config/nvim"
 	log cp -r "config/nvim" "$HOME/.config/"
@@ -73,6 +72,8 @@ nvim() {
 # nvim_clear #
 nvim_clear() {
 	[ -d $HOME/.local/share/nvim/ ] &&  log mv $HOME/.local/share/nvim $HOME/.local/share/nvim_$datetime
+	log rm -rf $HOME/.cache/nvim
+	log rm -rf $HOME/.local/state/nvim
 }
 
 
