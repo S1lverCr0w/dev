270349274a4ba55ba9571a2ec866a647f9abba03
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2026-02-03 17:52:10 +0000

    nvim moved the tresitter into the plugins directory and merged kickstart and other guy's lsp setup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,6 @@
 require("config.lazy")
 require("keymaps")
-require("treesitter.treesitter_setup").setup()
+-- require("treesitter.treesitter_setup").setup()
 -- require("java.jdtls_cmd") -- java jdtls eclipese lsp
 
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -3,4 +3,55 @@ return {
 	lazy = false,
 	branch = "main",
 	build = ":TSUpdate",
+
+	config = function()
+		local filetypes = {
+			"c",
+			"zig",
+			"assembly",
+			"fortran",
+			"java",
+			"python",
+			"bash",
+			-- "cpp",
+			-- "c_sharp",
+			-- "javascript",
+			-- "html",
+			-- "css",
+			-- "kotlin",
+			-- "cmake",
+			"make",
+			-- "php",
+			"lua",
+			-- "rust",
+			-- "json",
+			-- "go",
+			"markdown",
+			"markdown_inline",
+			-- "csv",
+			-- "diff",
+			-- "dockerfile",
+			-- "gitignore", -- causes an error
+			-- "typescript",
+			-- "yaml",
+			"vim",
+			"vimdoc",
+			"luadoc",
+			"diff",
+		}
+		require("nvim-treesitter.install").compilers = {
+			"clang",
+			"gcc",
+			"cc",
+			"cl",
+		}
+		require("nvim-treesitter").install(filetypes)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = filetypes,
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
 }
