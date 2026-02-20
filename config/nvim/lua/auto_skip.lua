cc916d8b66b616af289ae40a5c47c74e4a90fdba
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2026-02-20 21:02:27 +0000

    it is now aplpplied to certain filetypes only

--- config/nvim/lua/auto_skip.lua
+++ config/nvim/lua/auto_skip.lua
@@ -9,15 +9,28 @@ local keys = {
 	-- [" "] = " ", -- skip spaces
 }
 
-for key, char in pairs(keys) do
-	vim.keymap.set("i", key, function()
-		local col = vim.api.nvim_win_get_cursor(0)[2]
-		local line = vim.api.nvim_get_current_line()
-		local next_char = line:sub(col + 1, col + 1)
+vim.api.nvim_create_autocmd("FileType", {
+	-- apply only to these filestypes
+	pattern = {
+		"c",
+		"zig",
+		"java",
+		"python",
+		"lua",
+	},
+	callback = function()
+		for key, char in pairs(keys) do
+			vim.keymap.set("i", key, function()
+				-- _ is a throwaway variable as we only need the column where the cursor is (_ represents the row)
+				local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+				local line = vim.api.nvim_get_current_line()
+				local next_char = line:sub(col + 1, col + 1)
 
-		if next_char == char then
-			return "<Right>" -- move the cursor to the right one char
+				if next_char == char then
+					return "<Right>" -- move the cursor to the right one char
+				end
+				return key
+			end, { expr = true, buffer = true })
 		end
-		return key
-	end, { expr = true })
-end
+	end,
+})
