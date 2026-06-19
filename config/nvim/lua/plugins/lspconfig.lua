ebf259d6a16fb48b06dba12f32a82959dc0e33b1
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2026-06-19 16:20:47 +0100

    nvim 0.12 v004 minor lsp changes

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,39 +1,9 @@
 local function gh(repo) return 'https://github.com/' .. repo end
 
--- [[ LSP Configuration ]]
--- Brief aside: **What is LSP?**
---
--- LSP is an initialism you've probably heard, but might not understand what it is.
---
--- LSP stands for Language Server Protocol. It's a protocol that helps editors
--- and language tooling communicate in a standardized fashion.
---
--- In general, you have a "server" which is some tool built to understand a particular
--- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
--- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
--- processes that communicate with some "client" - in this case, Neovim!
---
--- LSP provides Neovim with features like:
---  - Go to definition
---  - Find references
---  - Autocompletion
---  - Symbol Search
---  - and more!
---
--- Thus, Language Servers are external tools that must be installed separately from
--- Neovim. This is where `mason` and related plugins come into play.
---
--- If you're wondering about lsp vs treesitter, you can check out the wonderfully
--- and elegantly composed help section, `:help lsp-vs-treesitter`
-
 -- Useful status updates for LSP.
 vim.pack.add { gh 'j-hui/fidget.nvim' }
 require('fidget').setup {}
 
---  This function gets run when an LSP attaches to a particular buffer.
---    That is to say, every time a new file is opened that is associated with
---    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
---    function will be executed to configure the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
 	group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
 	callback = function(event)
@@ -99,24 +69,22 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 -- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---  See `:help lsp-config` for information about keys and how to configure
 ---@type table<string, vim.lsp.Config>
 local servers = {
 	clangd = {},
 	zls = {},
 	fortls = {},
 	jdtls = {},
-	-- bashls = {},
+	bashls = {},
 	-- gopls = {},
 	-- pyright = {},
 	-- rust_analyzer = {},
+	-- ts_ls = {},
 	--
 	-- Some languages (like typescript) have entire language plugins that can be useful:
 	--    https://github.com/pmizio/typescript-tools.nvim
-	--
-	-- But for many setups, the LSP (`ts_ls`) will work just fine
-	-- ts_ls = {},
+
+	-- python modern alternatives to pyright ty and ruff
 	ty = {
 		cmd = { 'ty', 'server' },
 		filetypes = { 'python' },
@@ -166,6 +134,7 @@ local servers = {
 
 vim.pack.add {
 	gh 'neovim/nvim-lspconfig',
+	-- -- remove mason
 	-- gh 'mason-org/mason.nvim',
 	-- gh 'mason-org/mason-lspconfig.nvim',
 	-- gh 'WhoIsSethDaniel/mason-tool-installer.nvim',
@@ -174,11 +143,7 @@ vim.pack.add {
 -- Automatically install LSPs and related tools to stdpath for Neovim
 -- require('mason').setup {}
 
--- Ensure the servers and tools above are installed
---
--- To check the current status of installed tools and/or manually install
--- other tools, you can run
---    :Mason
+-- To check the current status of installed tools and/or manually install other tools, you can run :Mason
 --
 -- You can press `g?` for help in this menu.
 local ensure_installed = vim.tbl_keys(servers or {})
