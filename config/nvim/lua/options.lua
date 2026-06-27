2848d9a4cfd5219a8898ef28f91cbca88024f296
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2026-06-27 01:30:52 +0100

    nvim 0.12 v006 completion pop-u colour change

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -25,3 +25,22 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
 	callback = function() vim.hl.on_yank() end,
 })
+
+-- completion pop-up color change to maek it stand out from the bg -- maybe add to onedark setup??
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		-- -- main completion menu
+		-- vim.api.nvim_set_hl(0, "BlinkCmpMenu", { bg = "#0c0d10", fg = "#abb2bf" }) -- deep abyss
+		-- vim.api.nvim_set_hl(0, "BlinkCmpMenu", { bg = "#121317", fg = "#abb2bf" }) -- defined constrast
+		vim.api.nvim_set_hl(0, "BlinkCmpMenu", { bg = "#16181c", fg = "#abb2bf" }) -- subtle contrast
+		-- vim.api.nvim_set_hl(0, "BlinkCmpMenu", { bg = "#1f232a", fg = "#abb2bf" }) -- brighter/greyish
+
+		-- -- menu Border
+		-- vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { bg = "#1f232a", fg = "#4b5263" })
+
+		-- -- the currently selected item in the list
+		vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { bg = "#2c313c" })
+		-- vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { bg = "#2c313c", fg = "#ffffff", bold = true })
+	end,
+})
