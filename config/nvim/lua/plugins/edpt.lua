7c02627bb5e52fec7310ca69972d0b026ebc0aa9
S1lverCr0w <36712827+S1lverCr0w@users.noreply.github.com>
2026-07-15 00:47:28 +0100

    nvim modular theme setup 01

--- config/nvim/lua/plugins/edpt.lua
+++ config/nvim/lua/plugins/edpt.lua
@@ -6,7 +6,17 @@ local function gh(repo) return 'https://github.com/' .. repo end
 
 
 -- https://github.com/datsfilipe/nvim-colorscheme-template/tree/main
--- vim.pack.add { { src = gh 'S1lverCr0w/edpt.nvim', version = 'dev', } }
+
+vim.pack.add { { src = gh 'S1lverCr0w/edpt.nvim', version = "devel", } }
+-- require("onedark").setup({
+-- 	-- style = 'light',
+-- 	-- style = 'dark',
+-- })
+-- require("onelight").setup()
+-- require("onedark").setup()
+vim.cmd.colorscheme 'onedark'
+-- vim.cmd.colorscheme 'onelight'
+
 -- require('e_onedark').setup({
 -- 	-- theme = 'light',
 -- 	theme = 'dark',
@@ -14,22 +24,21 @@ local function gh(repo) return 'https://github.com/' .. repo end
 -- })
 -- vim.cmd.colorscheme 'e_onedark'
 
-
-vim.pack.add { gh 'S1lverCr0w/onedark.nvim' }
-require("onedarkpro").setup({
-	colors = {
-		onedark = { bg = '#1b1d22', }, -- enryu bg 0
--- 		-- onedark = { bg = '#16181d', }, -- enryu bg 1
-		onelight = { bg = "#f5f5f5" }, -- minimalist paper
-		-- onelight = { bg = "#e6e6e6" }, -- 90% value
-		-- onelight = { bg = "#d9d9d9" }, -- 85% value
-		-- onelight = { bg = "#eff1f5" }, -- soft cool gray
-	},
--- 	filetypes = { false },
--- 	-- plugins = { false },
--- 	plugins = { false, treesitter = { true }, },
-})
-
-require 'OneDarkProDarkify'						-- custom script to modfy the onedarkpro theme
-vim.cmd.colorscheme 'onedark'
+-- vim.pack.add { gh 'S1lverCr0w/onedark.nvim' }
+-- require("onedarkpro").setup({
+-- 	colors = {
+-- 		onedark = { bg = '#1b1d22', }, -- enryu bg 0
+-- -- 		-- onedark = { bg = '#16181d', }, -- enryu bg 1
+-- 		onelight = { bg = "#f5f5f5" }, -- minimalist paper
+-- 		-- onelight = { bg = "#e6e6e6" }, -- 90% value
+-- 		-- onelight = { bg = "#d9d9d9" }, -- 85% value
+-- 		-- onelight = { bg = "#eff1f5" }, -- soft cool gray
+-- 	},
+-- -- 	filetypes = { false },
+-- -- 	-- plugins = { false },
+-- -- 	plugins = { false, treesitter = { true }, },
+-- })
+--
+-- require 'OneDarkProDarkify'						-- custom script to modfy the onedarkpro theme
+-- vim.cmd.colorscheme 'onedark'
 -- vim.cmd.colorscheme 'onelight'
--- config/nvim/lua/plugins/onedark.lua
+++ config/nvim/lua/plugins/onedark.lua
@@ -1,14 +1,22 @@
 local function gh(repo) return 'https://github.com/' .. repo end
 
-vim.pack.add { gh 'S1lverCr0w/onedark.nvim' }
-require("onedarkpro").setup({
-	colors = {
-		onedark = { bg = "#1b1d22" },
-		onelight = { bg = "#f5f5f5" }, -- minimalist paper
-		-- onelight = { bg = "#e6e6e6" }, -- 90% value
-		-- onelight = { bg = "#d9d9d9" }, -- 85% value
-		-- onelight = { bg = "#eff1f5" }, -- soft cool gray
-	}
+-- -- old colorscheme experiment with difeerent onedark fork that alrady has darkmode/lightmode
+-- vim.pack.add { gh 'S1lverCr0w/onedark.nvim' }
+-- require("onedarkpro").setup({
+-- 	colors = {
+-- 		onedark = { bg = "#1b1d22" },
+-- 		onelight = { bg = "#f5f5f5" }, -- minimalist paper
+-- 		-- onelight = { bg = "#e6e6e6" }, -- 90% value
+-- 		-- onelight = { bg = "#d9d9d9" }, -- 85% value
+-- 		-- onelight = { bg = "#eff1f5" }, -- soft cool gray
+-- 	}
+-- })
+
+vim.pack.add { gh 'S1lverCr0w/edpt.nvim' }
+require("onedark").setup({
+	-- style = 'light',
+	style = 'dark',
 })
+
 vim.cmd.colorscheme 'onedark'
 -- vim.cmd.colorscheme 'onelight'
